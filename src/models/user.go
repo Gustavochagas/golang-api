5772a42c062956a8cf405a7b000894cc40cb99df
Gustavo Chagas <gustavochagasb@gmail.com>
2024-08-26 19:20:08 -0300

    Validate user fields

--- src/models/user.go
+++ src/models/user.go
@@ -1,6 +1,10 @@
 package models
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 type User struct {
 	ID        uint64    `json:"id,omitempty"`
@@ -9,4 +13,39 @@ type User struct {
 	Email     string    `json:"email,omitempty`
 	Password  string    `json:"password,omitempty`
 	CreatedAt time.Time `json:"created_at,omitempty`
+}
+
+func (user *User) Prepare() error {
+	if erro := user.validate(); erro != nil {
+		return erro
+	}
+
+	user.format()
+	return nil
+}
+
+func (user *User) validate() error {
+	if user.Name == "" {
+		return errors.New("ame is required")
+	}
+
+	if user.Nick == "" {
+		return errors.New("Nick is required")
+	}
+
+	if user.Email == "" {
+		return errors.New("Email is required")
+	}
+
+	if user.Password == "" {
+		return errors.New("Password is required")
+	}
+
+	return nil
+}
+
+func (user *User) format() {
+	user.Name = strings.TrimSpace(user.Name)
+	user.Nick = strings.TrimSpace(user.Nick)
+	user.Email = strings.TrimSpace(user.Email)
 }
